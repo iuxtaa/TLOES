362ed69a9630237920695226bddf1191a98cf141
Enab26 <anab.ismail2606@gmail.com>
2024-04-05 01:38:23 +1300

    0
     I dedicated my time to developing the script for the Inventory User Interface (UI) page. This task involved carefully planning and adjusting the various items that will be integral to the game's inventory system. To improve the user experience, I also introduced a 'close' button within the inventory UI

---
+++ TLOES/Assets/Assets/InventoryUI/InventoryUi.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUi : MonoBehaviour
+{
+    [SerializeField] InventoryUi Coinprefab;
+    [SerializeField] RectTransform contentPanel; 
+
+
+
+
+}
