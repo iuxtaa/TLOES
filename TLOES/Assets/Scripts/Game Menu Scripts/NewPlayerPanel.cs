b325953997e6008305fe9324d41a64370ec081ad
yza <yzapernia@outlook.com>
2024-04-26 11:48:40 +1200

    Revert "Fixed github issues"
    This reverts commit 549a7bca465bd79e5e95003b384939b7d264f17e.

---
+++ TLOES/Assets/Scripts/Game Menu Scripts/NewPlayerPanel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewPlayerPanel : MonoBehaviour
+{
+    // Variable buttons of the new player panel
+    public Button newGameButton;
+    public Button quitGameButton;
+}
---
+++ TLOES/Assets/Scripts/Starting Menu Scripts/StartingPanelManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingPanelManager : MonoBehaviour
+{
+    [SerializeField] private Panel startingPanel;
+    [SerializeField] private GameObject loginPanel;
+    [SerializeField] private GameObject signupPanel;
+    
+    public static int menuChoice = (int)StartingMenuChoice.Unspecified;
+    public void Start() // If scene is generated
+    {
+        startingPanel.loginButton.onClick.AddListener(LogInClicked);
+        startingPanel.signUpButton.onClick.AddListener(SignUpClicked);
+        
+    }
+
+    private void SignUpClicked()
+    {
+        HandleStartingPanel();
+        signupPanel.SetActive(true);
+        menuChoice = (int)StartingMenuChoice.Signup;
+    }
+
+    private void LogInClicked()
+    {
+        HandleStartingPanel();
+        loginPanel.SetActive(true);
+        menuChoice = (int)StartingMenuChoice.Login;
+    }
+
+    private void HandleStartingPanel()
+    {
+        startingPanel.gameObject.SetActive(false);
+    }
+}
+
+public enum StartingMenuChoice {
+    Unspecified,
+    Signup,
+    Login
+}
