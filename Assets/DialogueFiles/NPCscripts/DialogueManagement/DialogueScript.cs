8e13c0056704e1b9403abdbb9e917595dfd8b97b
kalanda4real <142495537+kalanda4real@users.noreply.github.com>
2024-04-26 14:57:01 +1200

    bug update after merges

--- Assets/DialogueFiles/NPCscripts/DialogueManagement/DialogueScript.cs
+++ Assets/DialogueFiles/NPCscripts/DialogueManagement/DialogueScript.cs
@@ -131,7 +131,8 @@ public class DialogueScript : MonoBehaviour
         {
             choices[i].gameObject.SetActive(false);
         }
-        StartCoroutine(SelectedFristChoice()); 
+
+        StartCoroutine(SelectedFristChoice());
     }
 
     private IEnumerator SelectedFristChoice()
