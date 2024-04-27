25b480d254e068383af02401efca91cf64c1eb06
iuxtaa <anngabrielledelrosario@gmail.com>
2024-04-27 18:50:45 +1200

    Added questing files and fixed font name errors

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,6 +6,7 @@ public class Player : Character
 {
     // INSTANCE VARIABLE
     private int favourability;
+    [SerializeField] public Quest currentQuest;
 
     // CONSTRUCTOR
     public Player(string name) : base(name)
@@ -26,5 +27,53 @@ public class Player : Character
     public int GetFavourability()
     {
         return this.favourability;
+    }
+
+    public void acceptQuest(Quest quest)
+    {
+        currentQuest = quest;
+        currentQuest.isActive = true;
+    }
+
+    public void declineQuest()
+    {
+
+    }
+
+    public bool canCompleteQuest()
+    {
+        if (currentQuest != null)
+        {
+            if (currentQuest is SellingQuest)
+            {
+                // return (inventory.item.count >= requiredAmount)
+            }
+            if (currentQuest is DoingQuest)
+            {
+                // return true;
+            }
+            if (currentQuest is CollectingQuest)
+            {
+                // return (inventory.item.count >= requiredAmount)
+            }
+        }
+        return false;
+    }
+
+    public void completeQuest()
+    {
+        if (canCompleteQuest())
+        {
+            favourability += currentQuest.favourabilityReward;
+            currentQuest.complete();
+            currentQuest = null;
+        }
+    }
+
+    public void failQuest()
+    {
+        favourability -= currentQuest.favourabilityReward;
+        currentQuest.complete();
+        currentQuest = null;
     }
 }
