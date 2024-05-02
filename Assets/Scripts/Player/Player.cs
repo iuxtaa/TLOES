338c853ac4b0031ef73db6f0fdfb260aa5b69f8c
kalanda4real <142495537+kalanda4real@users.noreply.github.com>
2024-05-03 10:53:47 +1200

    inventory bug commented out

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -82,11 +82,11 @@ public class Player : Character {
             favourability += currentQuest.favourabilityReward;
             if (currentQuest is CollectingQuest collectingQuest)
             {
-                RemoveItem(collectingQuest.requiredItem.name, collectingQuest.requiredAmount);
+              //  RemoveItem(collectingQuest.requiredItem.name, collectingQuest.requiredAmount);
             }
             else if (currentQuest is SellingQuest sellingQuest)
             {
-                RemoveItem(sellingQuest.requiredItem.name, sellingQuest.requiredAmount);
+               // RemoveItem(sellingQuest.requiredItem.name, sellingQuest.requiredAmount);
             }
             currentQuest.complete();
             SetQuest(null);
