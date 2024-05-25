db8918dfb63c1e05f0f73dfc34b3d079bd06d84a
kalanda4real <142495537+kalanda4real@users.noreply.github.com>
2024-05-25 17:46:45 +1200

    Added npcs in tutorial scene

--- Assets/Scripts/Inventory/CollectablesCode/Inventory.cs
+++ Assets/Scripts/Inventory/CollectablesCode/Inventory.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
+//[System.Serializable]
 public class Inventory 
 {
-    [System.Serializable]
+   // [System.Serializable]
     public class Slot
     {
         // CONSTANT VARIABLES
