e9f9dab056830b58b7c695143e1e916a1259db2f
kalanda4real <142495537+kalanda4real@users.noreply.github.com>
2024-06-03 21:34:11 +1200

    quest overlay manager updated

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -37,6 +37,10 @@ public class UIManager : MonoBehaviour
         {
             questOverlay.SetActive(true);
         }
+        else
+        {
+            questOverlay.SetActive(false);
+        }
     }
 
     #region Pause
